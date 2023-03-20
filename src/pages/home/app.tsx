a9e36029d836534d6178d3eba3a96f8662e3e4e3
Jason Yaeger <jason@tenacitycloud.com>
2023-03-20 14:34:07 -0400

    updates to home

--- src/pages/home/app.tsx
+++ src/pages/home/app.tsx
@@ -1,10 +1,53 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 import React from 'react';
+import TopNavigation from '@cloudscape-design/components/top-navigation';
+import AppLayout from '@cloudscape-design/components/app-layout';
+import Navigation from './components/navigation';
+import Breadcrumbs from './components/breadcrumbs';
+import ContentLayout from '@cloudscape-design/components/content-layout';
+import Container from '@cloudscape-design/components/container';
+import Header from '@cloudscape-design/components/header';
+import PreparedDashboardContent from './components/prepared-dashboard-content';
+
+
+import './styles.css';
+
 export default function App() {
   return (
     <>
-      Hello <a href="https://cloudscape.design/">Cloudscape Design System</a>!
+      <div id="top-nav">
+        <TopNavigation
+          identity={{
+            logo: { src: '/logo.svg', alt: 'Tenacity | Cloud Spend Management Platform' },
+            title: 'Cloud FinOps',
+            href: '/home/index.html',
+          }}
+          i18nStrings={{
+            overflowMenuTriggerText: 'More',
+            overflowMenuTitleText: 'All',
+          }}
+        />
+      </div>
+      <AppLayout
+        headerSelector="#top-nav"
+        ariaLabels={{
+          navigation: 'Navigation drawer',
+          navigationClose: 'Close navigation drawer',
+          navigationToggle: 'Open navigation drawer',
+          notifications: 'Notifications',
+          tools: 'Help panel',
+          toolsClose: 'Close help panel',
+          toolsToggle: 'Open help panel',
+        }}
+        navigation={<Navigation />}
+        breadcrumbs={<Breadcrumbs />}
+        content={
+          <ContentLayout header={<Header variant="h1">Dashboard</Header>}>
+           <PreparedDashboardContent />
+          </ContentLayout>
+        }
+      />
     </>
   );
 }
--- src/pages/home/components/navigation.tsx
+++ src/pages/home/components/navigation.tsx
@@ -1,7 +1,26 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 import React from 'react';
+import SideNavigation, { SideNavigationProps } from '@cloudscape-design/components/side-navigation';
+
+const items: SideNavigationProps['items'] = [
+  // More pages got added as part of the workshop.
+  { type: 'link', text: 'Dashboard', href: '/home/index.html' },
+  { type: 'link', text: 'Savings', href: '/home/index.html' },
+  { type: 'link', text: 'Commitments', href: '/home/index.html' },
+  { type: 'link', text: 'Alerting', href: '/home/index.html' },
+  { type: 'link', text: 'Allocations', href: '/home/index.html' },
+  { type: 'link', text: 'Reporting', href: '/home/index.html' },
+];
 
 export default function Navigation() {
-  return (<></>);
+  return (
+    <>
+      <SideNavigation
+        activeHref={location.pathname}
+        header={{ href: '/home/index.html', text: 'Service' }}
+        items={items}
+      />
+    </>
+  );
 }
